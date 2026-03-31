9c0c3fcae0201be5263f0c1598078c0171362c92
SangHyouk Jin <chingoo2@naver.com>
2026-03-31 18:42:37 +0900

    Initial Commit

---
+++ DizzyFlow/DizzyFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DizzyFlowApp.swift
+//  DizzyFlow
+//
+//  Created by SangHyouk Jin on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct DizzyFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
